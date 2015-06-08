5aa6951b1cc8f25d0b4093f5e1c66831a1d81930
mpallansch <mkp95@uga.edu>
2015-06-08 15:39:23 -0400

    Adds ElementWave

---
+++ src/com/example/testapp/DeepLink.java
@@ -0,0 +1,15 @@
+package com.example.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DeepLink extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_deep_link);
+	}
+}
