63b3b4f65885efd967d0fd88fe8dbbe27acf0f40
mpallansch <mkp95@uga.edu>
2015-07-10 10:14:26 -0400

    Revert "Adds ElementWave"
    This reverts commit 5aa6951b1cc8f25d0b4093f5e1c66831a1d81930.

--- src/com/example/testapp/MainActivity.java
+++ src/com/example/testapp/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.testapp;
 
-import com.elementwave.android.ElementWave;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,30 +10,6 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		ElementWave.init(this, "1050338525390", "aacac109", "0f5f07dc24");
-		
-		String web = getIntent().getStringExtra(ElementWave.NOTIFICATION_WEB);
-		if(web != null){
-			Intent deep = new Intent(this, DeepLink.class);
-			deep.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			startActivity(deep);
-		}
-		
 		setContentView(R.layout.activity_main);
-	}
-	
-	@Override
-	public void onResume()
-	{
-	   super.onResume();
-	   ElementWave.onResume();
-	}
-
-	@Override
-	public void onPause()
-	{
-	   super.onPause();
-	   ElementWave.onPause();
 	}
 }
