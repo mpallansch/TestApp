aed436a1f32273a78dad399a220ef99522961201
mpallansch <mkp95@uga.edu>
2015-07-14 11:48:35 -0400

    Adds Adobe Configuration

--- src/com/example/testapp/MainActivity.java
+++ src/com/example/testapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.testapp;
 
+import com.adobe.mobile.Analytics;
+import com.adobe.mobile.Config;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,5 +14,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Config.setContext(getApplicationContext());
+		Analytics.trackAction("Application Launched", null);
 	}
 }
